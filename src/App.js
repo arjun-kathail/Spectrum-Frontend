7d38e44566756450dbf064566c30d37771f8d50c
Arjun Kathail <arjunkathail5@gmail.com>
2023-03-10 04:58:33 +0530

    Navbar template addition

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import styles from './App.module.css';
+// import styles from './App.module.css';
 import Landing from './Landing';
+import NavBar from './Components/Navbar'
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 function App() {
-  return <div className={styles.body}>
+  return <ThemeProvider theme={darkTheme}>
+    <CssBaseline />
+    <NavBar />
     <Landing />
-  </div>;
+  </ThemeProvider>;
 }
 
 export default App;
