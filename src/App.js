cd2f134da085532740bca7d241b1a4df1b15e90c
priyanshukaushal <priyanshu.kaushal2001@gmail.com>
2023-03-11 01:11:30 +0530

    Added google login

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Landing from './Landing';
 import NavBar from './Components/Navbar'
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const darkTheme = createTheme({
   palette: {
@@ -12,11 +13,15 @@ const darkTheme = createTheme({
 });
 
 function App() {
-  return <ThemeProvider theme={darkTheme}>
+  return <GoogleOAuthProvider clientId="794358287288-hdchp559q23p7piav6qlnj1dsml2pvnn.apps.googleusercontent.com">
+    <React.StrictMode>
+    <ThemeProvider theme={darkTheme}>
     <CssBaseline />
     <NavBar />
     <Landing />
-  </ThemeProvider>;
+    </ThemeProvider>
+    </React.StrictMode>
+  </GoogleOAuthProvider>;
 }
 
 export default App;
---
+++ src/Components/GoogleLogin/index.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { googleLogout, useGoogleLogin } from '@react-oauth/google';
+import axios from 'axios';
+
+const GoogleLoginButton = () => {
+    const [ user, setUser ] = useState([]);
+    const [ profile, setProfile ] = useState([]);
+
+    const login = useGoogleLogin({
+        onSuccess: (codeResponse) => setUser(codeResponse),
+        onError: (error) => console.log('Login Failed:', error)
+    });
+
+    useEffect(
+        () => {
+            if (user) {
+                axios
+                    .get(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
+                        headers: {
+                            Authorization: `Bearer ${user.access_token}`,
+                            Accept: 'application/json'
+                        }
+                    })
+                    .then((res) => {
+                        setProfile(res.data);
+                    })
+                    .catch((err) => console.log(err));
+            }
+        },
+        [ user ]
+    );
+
+    const logOut = () => {
+        googleLogout();
+        setProfile(null);
+    };
+
+    return (
+        <div>
+            {profile ? (
+                <button onClick={logOut}>Log out</button>
+            ) : (
+                <button onClick={() => login()}>Sign in with Google</button>
+            )}
+        </div>
+    );
+}
+export default GoogleLoginButton;
