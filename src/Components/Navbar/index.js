2719a5e7b853e4a4a2b4444935a21a88802d93c0
Arjun Kathail <arjunkathail5@gmail.com>
2023-03-21 05:19:56 +0530

    Profile picture crash temporary fix

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -19,142 +19,142 @@ const pages = ['Team', 'Developers', 'Contact Us'];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 function NavBar(props) {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
 
-  return (
-    <AppBar
-      position='absolute'
-      sx={{
-        background: '#000',
-        opacity: 0.75,
-        backdropFilter: 'blur(5px) saturate(180%)',
-        webkitBackdropFilter: 'blur(5px) saturate(180%)',
-      }}
-      enableColorOnDark
-    >
-      <Container maxWidth='x1'>
-        <Toolbar disableGutters>
-          <Box
-            component='img'
-            alt='Spectrum Logo'
-            src={SpectrumLogo}
-            sx={{ height: '1.5rem', display: { xs: 'none', md: 'flex' }, mr: 1 }}
-          />
+    return (
+        <AppBar
+            position='absolute'
+            sx={{
+                background: '#000',
+                opacity: 0.75,
+                backdropFilter: 'blur(5px) saturate(180%)',
+                webkitBackdropFilter: 'blur(5px) saturate(180%)',
+            }}
+            enableColorOnDark
+        >
+            <Container maxWidth='x1'>
+                <Toolbar disableGutters>
+                    <Box
+                        component='img'
+                        alt='Spectrum Logo'
+                        src={SpectrumLogo}
+                        sx={{ height: '1.5rem', display: { xs: 'none', md: 'flex' }, mr: 1 }}
+                    />
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size='large'
-              aria-label='account of current user'
-              aria-controls='menu-appbar'
-              aria-haspopup='true'
-              onClick={handleOpenNavMenu}
-              color='white'
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id='menu-appbar'
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign='center' fontFamily='Ubuntu'>
-                    {page}
-                  </Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <Box
-            component='img'
-            alt='Spectrum Logo'
-            src={SpectrumLogo}
-            sx={{ height: '1.2rem', display: { xs: 'flex', md: 'none' }, mr: 3 }}
-          />
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, ml: 1 }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{
-                  my: 2,
-                  color: 'white',
-                  display: 'block',
-                  fontSize: '1rem',
-                  fontWeight: '600',
-                  fontFamily: 'Ubuntu',
-                }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size='large'
+                            aria-label='account of current user'
+                            aria-controls='menu-appbar'
+                            aria-haspopup='true'
+                            onClick={handleOpenNavMenu}
+                            color='white'
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id='menu-appbar'
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                    <Typography textAlign='center' fontFamily='Ubuntu'>
+                                        {page}
+                                    </Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <Box
+                        component='img'
+                        alt='Spectrum Logo'
+                        src={SpectrumLogo}
+                        sx={{ height: '1.2rem', display: { xs: 'flex', md: 'none' }, mr: 3 }}
+                    />
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, ml: 1 }}>
+                        {pages.map((page) => (
+                            <Button
+                                key={page}
+                                onClick={handleCloseNavMenu}
+                                sx={{
+                                    my: 2,
+                                    color: 'white',
+                                    display: 'block',
+                                    fontSize: '1rem',
+                                    fontWeight: '600',
+                                    fontFamily: 'Ubuntu',
+                                }}
+                            >
+                                {page}
+                            </Button>
+                        ))}
+                    </Box>
 
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title='Open settings'>
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt='Remy Sharp' src={props.user.picture} />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id='menu-appbar'
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign='center' fontFamily='Ubuntu'>
-                    {setting}
-                  </Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <GoogleLoginButton />
-        </Toolbar>
-      </Container>
-    </AppBar>
-  );
+                    <Box sx={{ flexGrow: 0 }}>
+                        <Tooltip title='Open settings'>
+                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                <Avatar alt='Remy Sharp' src={props?.user?.picture || "https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0025/1559/brand.gif?itok=vXujPldk"} />
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{ mt: '45px' }}
+                            id='menu-appbar'
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {settings.map((setting) => (
+                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                    <Typography textAlign='center' fontFamily='Ubuntu'>
+                                        {setting}
+                                    </Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <GoogleLoginButton />
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
 }
 export default NavBar;
