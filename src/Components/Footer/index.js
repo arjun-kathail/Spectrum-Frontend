d31107489f59ec4eb0ebe96887baab86d30407ec
priyanshukaushal <priyanshu.kaushal2001@gmail.com>
2023-04-06 00:07:16 +0530

    footer contact change

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -11,44 +11,47 @@ const Footer = (props) => {
     <div id='footer' className={styles.container}>
       <div>
         <p className={styles.heading}>Connect with us on:</p>
-        <a
-          className={styles.icon}
-          href='https://www.facebook.com/apc.pec'
-          target='_blank'
-          rel='noreferrer'
-        >
-          <FacebookIcon />
-        </a>
-        <a
-          className={styles.icon}
-          href='https://www.linkedin.com/company/art-photography-club/about/'
-          target='_blank'
-          rel='noreferrer'
-        >
-          <LinkedInIcon />
-        </a>
-        <a
-          className={styles.icon}
-          href='https://www.instagram.com/spectrum_apc/'
-          target='_blank'
-          rel='noreferrer'
-        >
-          <InstagramIcon />
-        </a>
-        <a
-          className={styles.icon}
-          href='mailto:spectrum.apcpec@gmail.com'
-          target='_blank'
-          rel='noreferrer'
-        >
-          <EmailIcon />
-        </a>
+        <div className={styles.iconContainer}>
+          <a
+            className={styles.icon}
+            href='https://www.facebook.com/apc.pec'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <FacebookIcon />
+          </a>
+          <a
+            className={styles.icon}
+            href='https://www.linkedin.com/company/art-photography-club/about/'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <LinkedInIcon />
+          </a>
+          <a
+            className={styles.icon}
+            href='https://www.instagram.com/spectrum_apc/'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <InstagramIcon />
+          </a>
+          <a
+            className={styles.icon}
+            href='mailto:spectrum.apcpec@gmail.com'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <EmailIcon />
+          </a>
+        </div>
         <p className={styles.text}>©Spectrum. All rights reserved.</p>
       </div>
       <div>
         <p className={styles.heading}>Contact details:</p>
-        <p className={styles.Contacttext}>Tushar Garg - 9115670588</p>
-        <p className={styles.Contacttext}>Richa Sabharwal - 8284003672</p>
+        <p className={styles.Contacttext}>Sarthak Chauhan - 8580665318</p>
+        <p className={styles.Contacttext}>Muskan Singh - 9530704884</p>
+        <p className={styles.Contacttext}>Ashlesha - 8837509462</p>
       </div>
     </div>
   );
